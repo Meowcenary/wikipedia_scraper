d662239ff6160982c37fb761abb2fa941fb32cc2
Eric Newman <newmaneric5@gmail.com>
2023-10-17 20:24:04 -0500

    Scrape Robotics Page to File
    - Add specific functions to scrape Wikipedia pages with Colly
    - Add json encoding to write out scraped data to files
    - Add link to json linter into README
    - Add .gitignore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wikipedia_scraper
 
 Wikipedia scraper written in Go
+
+Validate output with [JSON lint](https://jsonlint.com/)
--- main.go
+++ main.go
@@ -1,22 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
+// Struct to hold scraped data
+type WikiPage struct {
+	Url string
+	Title string
+	Text string
+	Tags []string
+}
+
+// Constructor
+func NewWikiPage(url string) *WikiPage {
+	return &WikiPage{
+		Url: url,
+		Title: "",
+		Text: "",
+		Tags: []string{},
+	}
+}
+
+func scrapeTitle(e *colly.HTMLElement) {
+	currentPage.Title = fmt.Sprintf("%s", e.Text)
+}
+
+func scrapeText(e *colly.HTMLElement) {
+	currentPage.Text = fmt.Sprintf("%s", e.Text)
+}
+
+func scrapeWikiUrls(urls []string, wikiPageCollector *colly.Collector) []WikiPage {
+	json := []WikiPage{}
+
+	for _, url := range urls {
+		// Assign currentPage pointer to a new WikiPage struct
+		currentPage = NewWikiPage(url)
+		// Visit url and trigger scraping functions
+		wikiPageCollector.Visit(url)
+		// Dereference and append WikiPage struct
+		json = append(json, *currentPage)
+	}
+
+	return json
+}
+
+// Pointer to WikiPage object, used to work around passing arguments to callbacks
+var currentPage *WikiPage
 func main() {
-	c := colly.NewCollector()
+	// Urls to scrape
+	urls := []string{
+		"https://en.wikipedia.org/wiki/Robotics",
+	}
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
+	// Colly
+	wikiPageCollector := colly.NewCollector()
+	wikiPageCollector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
+	wikiPageCollector.OnHTML("#firstHeading", scrapeTitle)
+	// #mw-content-text is taken from Python script "//div[@id="mw-content-text"]"
+	wikiPageCollector.OnHTML("#mw-content-text", scrapeText)
+	// wikiPageCollector is passed by reference to prevent instantiating it for each url
+	wikijson := scrapeWikiUrls(urls, wikiPageCollector)
 
-	c.Visit("http://go-colly.org/")
+	// Encode to json and write to file
+	filepath := "wikipages.json"
+	file, _ := os.Create(filepath)
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	enc.Encode(wikijson)
 }
